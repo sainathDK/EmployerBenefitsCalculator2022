eb7f486252ce8bc8a79c394804586c05f4079dbb
Sainath Kesavan <saikes+git@gmail.com>
2022-03-21 22:27:35 -0400

    added more tests

---
+++ Tests/BPCalcTests/DiscountForNameStarsWithARRuleTests.cs
@@ -0,0 +1,25 @@
+﻿using BPCalcAPI.Rules;
+using NUnit.Framework;
+using Shouldly;
+
+namespace BPCalcTests
+{
+    [TestFixture]
+    public class DiscountForNameStarsWithARRuleTests
+    {
+        [TestCase("Adam", 1000, 100)]
+        [TestCase("John", 1000, 0)]
+        [TestCase("Alice", 500, 50)]
+        public void Test_Discount_Calculation(string memberName,decimal benefitCost,decimal expectedDiscount)
+        {
+            //Arrange
+            var toTest = new DiscountForNameStarsWithARRule();
+
+            //Act
+            var resultFromRule = toTest.Compute(benefitCost, memberName);
+
+            //Assert
+            resultFromRule.ShouldBeEquivalentTo(expectedDiscount);
+        }
+    }
+}
